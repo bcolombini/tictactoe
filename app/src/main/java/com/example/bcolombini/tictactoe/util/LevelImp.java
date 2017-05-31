063f5509e89f3442a1eeb9fe572eb349e80d7742
bcolombini <bruno.colombini@me.com>
2017-05-31 00:58:54 -0300

    Refactoring

--- app/src/main/java/com/example/bcolombini/tictactoe/util/LevelImp.java
+++ app/src/main/java/com/example/bcolombini/tictactoe/util/LevelImp.java
@@ -3,11 +3,16 @@ package com.example.bcolombini.tictactoe.util;
 import android.text.TextUtils;
 import android.widget.Button;
 
+import com.example.bcolombini.tictactoe.util.configuration.PositionFinish;
+import com.example.bcolombini.tictactoe.util.general.Weight;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
 public class LevelImp implements LevelInterface {
+
+    PositionFinish positionFinish;
 
     private HashMap<String, Integer> position = new HashMap<>();
 
@@ -16,9 +21,6 @@ public class LevelImp implements LevelInterface {
     private ArrayList<String> humanHistory = new ArrayList<>();
 
     private HashMap<String, String> boardGame = new HashMap<>();
-
-    private ArrayList<Weight> winArray;
-
 
     @Override
     public void choiceLevel(int level) {
@@ -53,6 +55,8 @@ public class LevelImp implements LevelInterface {
             }
             boardGame.put(key, board.get(key).getText().toString());
         }
+
+        positionFinish = new PositionFinish(boardGame);
     }
 
     private HashMap<String, Integer> easy() {
@@ -68,7 +72,6 @@ public class LevelImp implements LevelInterface {
     //Futuro Medio
     private HashMap<String, Integer> medium() {
         HashMap<String, Integer> position = new HashMap<>();
-        positionToFinishGame();
 
         if (!boardGame.containsValue("o")) {
             if (boardGame.get("0-0").equals("x") || boardGame.get("0-2").equals("x") || boardGame.get("2-0").equals("x") || boardGame.get("2-2").equals("x")) {
@@ -81,7 +84,7 @@ public class LevelImp implements LevelInterface {
         calculateWeight(false);
 
         Weight betterWay = null;
-        for (Weight weight : winArray) {
+        for (Weight weight : positionFinish.getWinArray()) {
             if (betterWay == null) {
                 betterWay = weight;
             }
@@ -102,7 +105,6 @@ public class LevelImp implements LevelInterface {
     //Impossivel
     private HashMap<String, Integer> hard() {
         HashMap<String, Integer> position = new HashMap<>();
-        positionToFinishGame();
 
         if (!boardGame.containsValue("o")) {
             if (boardGame.get("0-0").equals("x") || boardGame.get("0-2").equals("x") || boardGame.get("2-0").equals("x") || boardGame.get("2-2").equals("x")) {
@@ -115,7 +117,7 @@ public class LevelImp implements LevelInterface {
         calculateWeight(false);
 
         Weight betterWay = null;
-        for (Weight weight : winArray) {
+        for (Weight weight : positionFinish.getWinArray()) {
             if (betterWay == null) {
                 betterWay = weight;
             }
@@ -146,76 +148,6 @@ public class LevelImp implements LevelInterface {
         return easy();
     }
 
-    private void positionToFinishGame() {
-        /*
-        0-0 - 1-0 - 2-0  X - -
-                         X - -
-                         X - -
-
-        0-1 - 1-1 - 2-1 - X -
-                        - X -
-                        - X -
-
-        2-2 - 1-2 - 0-2 - - X
-                        - - X
-                        - - X
-
-        0-0 - 0-1 - 0-2 X X X
-                        - - -
-                        - - -
-
-        1-0 - 1-1 - 1-2 - - -
-                        X X X
-                        - - -
-
-        2-2 - 2-1 - 2-0 - - -
-                        - - -
-                        X X X
-
-        2-0 - 1-1 - 0-2 - - X
-                        - X -
-                        X - -
-
-        0-0 - 1-1 - 2-2 X - -
-                        - X -
-                        - - X
-
-
-
-         */
-
-        winArray = null;
-        winArray = new ArrayList<>();
-        winArray.add(new Weight("0-0 - 1-0 - 2-0")); // Primeira Coluna
-        winArray.add(new Weight("0-1 - 1-1 - 2-1")); // Segunda Coluna
-        winArray.add(new Weight("0-2 - 1-2 - 2-2")); // Terceira Coluna
-
-        winArray.add(new Weight("0-0 - 0-1 - 0-2")); // Primeira Linha
-        winArray.add(new Weight("1-0 - 1-1 - 1-2")); // Segunda Linha
-        winArray.add(new Weight("2-2 - 2-1 - 2-0")); // Terceira Linha
-
-        winArray.add(new Weight("0-0 - 1-1 - 2-2")); // Diagonal \
-        winArray.add(new Weight("2-0 - 1-1 - 0-2")); // Diagonal /
-        removeIfLineIsComplete(winArray.size() - 1);
-    }
-
-    private void removeIfLineIsComplete(int i) {
-        if (i == -1) return;
-        int a = 0;
-        for (String key : boardGame.keySet()) {
-            if (!TextUtils.isEmpty(boardGame.get(key))) {
-                if (winArray.get(i).getMethod().contains(key)) a++;
-            }
-        }
-        if (a == 3) {
-            winArray.remove(i);
-            removeIfLineIsComplete(winArray.size() - 1);
-        } else {
-            removeIfLineIsComplete(i - 1);
-        }
-
-    }
-
     public void clearHistory() {
         computerHistory = new ArrayList<>();
         humanHistory = new ArrayList<>();
@@ -224,7 +156,7 @@ public class LevelImp implements LevelInterface {
     private void calculateWeight(boolean isComputer) {
         ArrayList<String> history = isComputer ? computerHistory : humanHistory;
         int sum = isComputer ? -1 : 1;
-        for (Weight weight : winArray) {
+        for (Weight weight : positionFinish.getWinArray()) {
             for (String cp : history) {
                 if (weight.getMethod().contains(cp)) {
                     weight.setCount(weight.getCount() + sum);
