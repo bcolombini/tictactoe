1dd838a9c7fa164bd33c8bc875a59fb68a2c111b
bcolombini <bruno.colombini@me.com>
2017-05-31 00:33:46 -0300

    Commit Imposible

--- app/src/main/java/com/example/bcolombini/tictactoe/util/LevelImp.java
+++ app/src/main/java/com/example/bcolombini/tictactoe/util/LevelImp.java
@@ -170,8 +170,8 @@ public class LevelImp implements LevelInterface {
         }
 
 
-        String[] splitado = betterWay.getMethod().split(" - ");
-        for (String sp : splitado) {
+        String[] splited = betterWay.getMethod().split(" - ");
+        for (String sp : splited) {
             if (TextUtils.isEmpty(boardGame.get(sp))) {
                 position.put("x", Integer.parseInt(sp.split("-")[0]));
                 position.put("y", Integer.parseInt(sp.split("-")[1]));
@@ -224,19 +224,54 @@ public class LevelImp implements LevelInterface {
 
         winArray = null;
         winArray = new ArrayList<>();
-        winArray.add(new Weight("0-0 - 1-0 - 2-0")); // Primeira Linha
-        winArray.add(new Weight("0-1 - 1-1 - 2-1")); // Segunda Linha
-        winArray.add(new Weight("2-2 - 1-2 - 0-2")); // Terceira Linha
-        winArray.add(new Weight("0-0 - 0-1 - 0-2")); // Primeira Coluna
-        winArray.add(new Weight("1-0 - 1-1 - 1-2")); // Segunda Coluna
-        winArray.add(new Weight("2-2 - 2-1 - 2-0")); // Terceira Coluna
-        winArray.add(new Weight("2-0 - 1-1 - 0-2")); // Diagonal /
+        winArray.add(new Weight("0-0 - 1-0 - 2-0")); // Primeira Coluna
+        winArray.add(new Weight("0-1 - 1-1 - 2-1")); // Segunda Coluna
+        winArray.add(new Weight("0-2 - 1-2 - 2-2")); // Terceira Coluna
+
+        winArray.add(new Weight("0-0 - 0-1 - 0-2")); // Primeira Linha
+        winArray.add(new Weight("1-0 - 1-1 - 1-2")); // Segunda Linha
+        winArray.add(new Weight("2-2 - 2-1 - 2-0")); // Terceira Linha
+
         winArray.add(new Weight("0-0 - 1-1 - 2-2")); // Diagonal \
+        winArray.add(new Weight("2-0 - 1-1 - 0-2")); // Diagonal /
+        removeIfLineIsComplete(winArray.size() - 1);
     }
+
+    private void removeIfLineIsComplete(int i) {
+        if (i == -1) return;
+        int a = 0;
+        for (String key : boardGame.keySet()) {
+            if (!TextUtils.isEmpty(boardGame.get(key))) {
+                if (winArray.get(i).getMethod().contains(key)) a++;
+            }
+        }
+        if (a == 3) {
+            winArray.remove(i);
+            removeIfLineIsComplete(winArray.size() - 1);
+        } else {
+            removeIfLineIsComplete(i - 1);
+        }
+
+    }
+
 
     public void clearHistory() {
         computerHistory = new ArrayList<>();
         humanHistory = new ArrayList<>();
+    }
+
+
+    private void calculateWeight(boolean isComputer) {
+        ArrayList<String> history = isComputer ? computerHistory : humanHistory;
+        int sum = isComputer ? -1 : 1;
+        for (Weight weight : winArray) {
+            for (String cp : history) {
+                if (weight.getMethod().contains(cp)) {
+                    weight.setCount(weight.getCount() + sum);
+                }
+            }
+        }
+        if (!isComputer) calculateWeight(true);
     }
 
 }
